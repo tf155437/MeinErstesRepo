31f70ca93a192d5172a4c5567a315421cd5d27dd
F.T <123622255+tf155437@users.noreply.github.com>
2023-10-19 06:37:23 +0000

    Weitere Speicherungen

---
+++ the art of nature/script.js
@@ -0,0 +1,63 @@
+//Product 1
+const Picture 1 =  {
+    articleID: 10001,
+    productname: 'Picture 1',
+    description: "Berge Boot See Haus Bäume Herbst", 
+    price: '56€',
+}
+
+//Product 2
+const Picture 2 =  {
+    articleID: 10002,
+    productname: 'Picture 2',
+    description: "wald Seee Frühling Bäume", 
+    price: '48€',
+}
+
+//Product 3
+const Picture 3 =  {
+    articleID: 10003,
+    productname: 'Picture 3',
+    description: "Berge Menschen Sonnenaufgang Menschen", 
+    price: '52€',
+}
+
+//Product 4
+const Picture 4 =  {
+    articleID: 10004,
+    productname: 'Picture 4',
+    description: "Felsen Strand Sand Bewölkt", 
+    price: '65€',
+}
+
+//Product 5
+const Picture 5 =  {
+    articleID: 10005,
+    productname: 'Picture 5',
+    description: "Sonnenuntergang Mond Berge Winter", 
+    price: '39€',
+}
+
+//Product 6
+const Picture 6 =  {
+    articleID: 10006,
+    productname: 'Picture 6',
+    description: "Winter Schweden Sonnenuntergang Wolken ", 
+    price: '45€',
+}
+
+//Product 7
+const Picture 7 =  {
+    articleID: 10007,
+    productname: 'Picture 7',
+    description: "Frühling Sonnenaufgang Berge", 
+    price: '65€',
+}
+
+//Product 8
+const Picture 8 =  {
+    articleID: 10008,
+    productname: 'Picture 8',
+    description: "Felder Sonnenuntergang Wolken ", 
+    price: '48€',
+}
